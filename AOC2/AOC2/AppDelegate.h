35727189f1b6de2fdb13a6bfc101be9666f390e4
b87775 <jwoods1@fullsail.edu>
2013-10-01 12:01:18 -0600

    First Commit

---
+++ AOC2/AOC2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AOC2
+//
+//  Created by Jason Woods on 10/1/13.
+//  Copyright (c) 2013 com.Fullsail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AOC2/AOC2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AOC2
+//
+//  Created by Jason Woods on 10/1/13.
+//  Copyright (c) 2013 com.Fullsail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
